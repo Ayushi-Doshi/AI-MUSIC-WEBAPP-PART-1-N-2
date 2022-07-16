f27f9c10ba2630d3e64a00f4b428c5a9fd2ad856
Ayushi-Doshi <85425548+Ayushi-Doshi@users.noreply.github.com>
2022-07-16 14:13:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 hms = "";
 us = "";
+lwx = 0;
+lwy = 0;
+rwx = 0;
+rwy = 0;
 
 function setup(){
     canvas = createCanvas(600, 500);
@@ -7,6 +11,8 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function draw(){
     image(video, 0 , 0, 600, 500);
@@ -17,4 +23,22 @@ function preload() {
 }
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
+function modelLoaded(){
+    console.log('PoseNet Is Intialized');
+}
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        lwx = results[0].pose.leftWrist.x;
+        lwy = results[0].pose.leftWrist.y;
+    console.log("leftWristX = " + lwx + "leftWristY = "+ lwy);
+    
+    rwx = results[0].pose.rightWrist.x;
+        rwy = results[0].pose.rightWrist.y;
+    console.log("rightWristX = " + rwx + "rightWristY = "+ rwy);
+    }
+    }
